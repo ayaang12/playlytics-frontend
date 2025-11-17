c85bfddfac7b0ccb868af0ba680452ac340780e4
neelypan <neel.pandruvada@gmail.com>
2025-11-16 20:21:27 -0500

    Fixed Styles and added author to the card - Ayaan

--- src/components/PlaylistCard.tsx
+++ src/components/PlaylistCard.tsx
@@ -2,17 +2,20 @@ import "./PlaylistCard.css";
 
 type Props = {
   name: string;
+  author: string;
   songs: number;
   minutes: number;
   image: string;
 };
 
-function PlaylistCard({ name, songs, minutes, image }: Props) {
+function PlaylistCard({ name, author, songs, minutes, image }: Props) {
   return (
     <div className="playlist-card">
       <img src={image} className="playlist-card-cover" />
 
       <p className="playlist-card-name">{name}</p>
+      <p className="playlist-card-author">{author}</p>
+
       <p className="playlist-card-info">
         {songs} songs | {minutes} min
       </p>
