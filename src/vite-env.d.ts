4257b54af20a357767e262355212cb1fdd049c3d
Neel Pandruvada <82175041+neelypan@users.noreply.github.com>
2025-11-21 14:45:54 -0500

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,7 +4,6 @@ declare interface ImportMetaEnv {
   readonly VITE_BACKEND_URL: string;
   readonly VITE_FRONTEND_API_KEY: string;
   readonly VITE_FUN_LITTLE_MESSAGE: string;
-  // add other VITE_... vars here as needed
 }
 
 declare interface ImportMeta {
