5a8718a18aac47dea20855765585603da4be012a
Ayaan Gill <ayaan.gill@mcvts.org>
2025-12-01 19:14:57 -0500

    randomize works now. Just had to react route it and yeah so

--- src/components/Randomize.tsx
+++ src/components/Randomize.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import type { PlaylistsData } from "./Playlists/playlistTypes";
+import "./Randomize.css";
 
 interface RandomizeProps {
   playlists: PlaylistsData[];
   setRandomPlaylist: React.Dispatch<React.SetStateAction<PlaylistsData | null>>;
 }
 
-const Randomize: React.FC<RandomizeProps> = ({
-  playlists,
-  setRandomPlaylist,
-}) => {
+const Randomize: React.FC<RandomizeProps> = ({ playlists, setRandomPlaylist }) => {
+  const navigate = useNavigate();
+
   const getRandomPlaylist = () => {
     if (Object.keys(playlists).length === 0) {
       return null;
@@ -17,41 +18,31 @@ const Randomize: React.FC<RandomizeProps> = ({
 
     const randPlaylist =
       playlists[Math.floor(Math.random() * Object.keys(playlists).length)];
-    console.log("Random Playlist:", randPlaylist);
 
     return randPlaylist;
   };
 
+  const handleRandom = () => {
+    const rand = getRandomPlaylist();
+    if (!rand) return;
+
+    setRandomPlaylist(rand);
+
+    navigate("/playlist-details", {
+      state: {
+        name: rand.name,
+        author: rand.author,
+        songs: rand.songAmnt,
+        minutes: rand.length,
+        image: rand.image,
+        spotifyUrl: rand.external_urls,
+      },
+    });
+  };
+
   return (
-    <button
-      onClick={() => {
-        setRandomPlaylist(getRandomPlaylist());
-      }}
-      style={{
-        position: "absolute",
-        top: "20px",
-        right: "20px",
-        background: "#353535",
-        border: "4px solid #1ed760",
-        borderRadius: "80px",
-        padding: "5px 20px",
-        height: "70px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        cursor: "pointer",
-      }}
-    >
-      <span
-        style={{
-          color: "white",
-          fontWeight: "800",
-          fontSize: "26px",
-          fontFamily: "League Spartan",
-        }}
-      >
-        RANDOMIZE PLAYLIST
-      </span>
+    <button className="randomize-button" onClick={handleRandom}>
+      <span className="randomize-text">RANDOMIZE PLAYLIST</span>
     </button>
   );
 };
